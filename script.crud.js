9eedd7b0568fef45ff6af3f6eb838f95561b7d32
Leticia <leticia.aragao00@gmail.com>
2023-11-29 18:39:44 +0100

    send files

---
+++ script.crud.js
@@ -0,0 +1,210 @@
+const taskListContainer = document.querySelector('.app__section-task-list');
+
+const formTask = document.querySelector('.app__form-add-task');
+const toggleFormTaskBtn = document.querySelector('.app__button--add-task');
+const formLabel = document.querySelector('.app__form-label');
+
+const textArea = document.querySelector('.app__form-textarea');
+
+const cancelBtn = document.querySelector('.app__form-footer__button--cancel');
+
+const deleteBtn = document.querySelector('.app__form-footer__button--delete');
+
+const clearTaskLisk = document.querySelector('#btn-remove-completed');
+
+const clearAlltaskList = document.querySelector('#btn-remove-all');
+
+const taskActiveDescription = document.querySelector('.app__section-active-task-description');
+const inProgressTask = document.querySelector('.app__section-active-task-label');
+
+
+const localStorageTask = localStorage.getItem('tasks');
+let tasks = localStorageTask ? JSON.parse(localStorageTask) : [];
+
+
+const taskIconSvg = `
+<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+    fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF" />
+    <path
+        d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+        fill="#01080E" />
+</svg>
+`
+let taskSelected = null;
+let itemTaskSelected = null;
+
+let editingTask = null;
+let editingParagrafh = null;
+
+function clearForm(){
+    editingTask = null;
+    editingParagrafh = null;
+    textArea.value = '';
+    formTask.classList.add('hidden');
+    taskActiveDescription.textContent = null;
+}
+
+const selectTask = (task, element) => {
+   document.querySelectorAll('.app__section-task-list-item-active').forEach(function (button){
+    button.classList.remove('.app__section-task-list-item-active');
+   })
+
+   if(taskSelected === task){
+    taskActiveDescription.textContent = null;
+    itemTaskSelected = null;
+    taskSelected = null;
+    return
+   }
+
+   taskSelected = task;
+   itemTaskSelected = element;
+   taskActiveDescription.textContent = task.description;
+   element.classList.add('.app__section-task-list-item-active');
+}
+
+const selectTaskToEdit = (task,element) => {
+    if(editingTask == task){
+        clearForm();
+        return;
+    }
+
+    formLabel.textContent = 'Editing task';
+    editingTask = task;
+    editingParagrafh = element;
+    textArea.value = task.description;
+    formTask.classList.remove('hidden');
+}
+
+
+function createTask(task){
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+
+    const svgIcon = document.createElement('svg');
+    svgIcon.innerHTML = taskIconSvg;
+
+    const paragrafh = document.createElement('p');
+    paragrafh.classList.add('app__section-task-list-item-description');
+    paragrafh.textContent = task.description;
+
+    const buttonSvg = document.createElement('button');
+    buttonSvg.classList.add('app_button-edit');
+    const editIcon = document.createElement('img');
+    editIcon.src = '/imagens/edit.png';
+
+    buttonSvg.appendChild(editIcon);
+
+    
+    svgIcon.addEventListener('click', (event) => {
+
+        if(task === taskSelected){
+            event.stopPropagation();
+            buttonSvg.setAttribute('disabled', true);
+            li.classList.add('app__section-task-list-item-complete');
+            taskSelected.concluded = true;
+            updateLocalStorage();
+        }
+    })
+
+    if(task.concluded){
+        buttonSvg.setAttribute('disabled', true);
+        li.classList.add('app__section-task-list-item-complete');
+    }
+
+    buttonSvg.onclick = (event) => {
+        event.stopPropagation();
+        selectTaskToEdit(task, paragrafh);
+    }
+
+    li.onclick = () => {
+        selectTask(task, li);
+    }
+    
+    li.appendChild(svgIcon);
+    li.appendChild(paragrafh);
+    li.appendChild(buttonSvg);
+
+    return li;
+}
+
+tasks.forEach(tarefa =>{
+    const taskItem = createTask(tarefa);
+    taskListContainer.appendChild(taskItem);
+})
+
+
+toggleFormTaskBtn.addEventListener('click', () => {
+    formLabel.textContent = 'Add task'; 
+    formTask.classList.toggle('hidden');
+})
+
+cancelBtn.addEventListener('click', clearForm);
+
+deleteBtn.addEventListener('click', () => {
+    if(taskSelected){
+        const index = tasks.indexOf(taskSelected);
+
+        if(index !== -1){
+            tasks.splice(index, 1);
+        }
+
+        itemTaskSelected.remove();
+        tasks.filter(tarefa=> tarefa!= taskSelected);
+        itemTaskSelected = null;
+        taskSelected = null;
+    }
+    updateLocalStorage();
+    clearForm();
+})
+
+const removeTasks = (justConcluded) => {
+    const selector = justConcluded ? '.app__section-task-list-item-complete' : '.app__section-task-list-item';
+    document.querySelectorAll(selector).forEach( (element) => {
+        element.remove();
+    })
+
+    tasks = justConcluded ? tasks.filter(task => !task.concluded) : [];
+    taskActiveDescription.textContent = null;
+    updateLocalStorage();
+}
+
+clearTaskLisk.addEventListener('click', () => removeTasks(true));
+
+clearAlltaskList.addEventListener('click', () => removeTasks(false));
+
+
+ function updateLocalStorage(){
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+ }
+
+formTask.addEventListener('submit', (evento) =>{
+    evento.preventDefault();
+    if(editingTask){
+        editingTask.description = textArea.value;
+        editingParagrafh.textContent = textArea.value;
+    }else{
+        const task = {
+            description: textArea.value,
+            concluded: false
+        };
+    
+    tasks.push(task);
+    const taskItem = createTask(task);
+    taskListContainer.appendChild(taskItem);
+    }
+    updateLocalStorage();
+    clearForm();
+})
+
+
+document.addEventListener('taskCompleted', function(e){
+    if(taskSelected){
+        taskSelected.concluded = true;
+        itemTaskSelected.classList.add('app__section-task-list-item-complete');
+        itemTaskSelected.querySelector('button').setAttribute('disabled', true);
+        updateLocalStorage();
+    }
+})
+
+
---
+++ script.js
@@ -0,0 +1,136 @@
+
+const titles = document.querySelectorAll('.app__title');
+const banner = document.querySelector('.app__image');
+const html = document.querySelector('html');
+const buttonsList = document.querySelectorAll('.app__card-button');
+const inputMusicFokus = document.querySelector('#alternar-musica');
+const startButton = document.querySelector('#start-pause');
+const startOrPauseButton = document.querySelector('#start-pause span');
+const imgPause = document.querySelector('.app__card-primary-button-icon');
+const screenTimer = document.querySelector('#timer');
+
+
+const music = new Audio('/sons/luna-rise-part-one.mp3');
+const soundPlay = new Audio('/sons/play.wav');
+const soundPause = new Audio('/sons/pause.mp3');
+const soundBeep = new Audio('/sons/beep.mp3');
+
+let secondsTimer = null;
+let timerBreak = null;
+
+
+function musicStatusChange(){
+    if(music.paused){
+        music.loop = true
+        music.currentTime = 5;
+        music.play();
+    } else{
+        music.pause();
+    }
+}
+
+inputMusicFokus.addEventListener('change', musicStatusChange);
+
+
+buttonsList.forEach(button => {
+    const buttonClass = button.classList[2];
+    button.onclick = function() {
+        html.setAttribute('data-contexto', buttonClass);
+        banner.src = `/imagens/${buttonClass}.png`
+
+        button.classList.add('active');
+
+        buttonsList.forEach(inactiveButton =>{
+            if(button != inactiveButton){
+                inactiveButton.classList.remove('active');
+            }
+        })
+
+
+        titles.forEach(title => {
+            const titleClass = title.classList[1];
+            if(titleClass === buttonClass){
+                title.classList.remove('hidden');
+            } else{
+                title.classList.add('hidden');
+            }
+           
+        });
+        const minutes = button.getAttribute('minutes');
+        secondsTimer = minutes * 60;
+        showTimer();
+    }
+});
+
+
+
+
+const countDown = () => {
+    if(secondsTimer <= 0){
+        // soundBeep.play();
+        stopCounter();
+        reset();
+        const focoAtivo = html.getAttribute('data-contexto') === 'foco'
+        if (focoAtivo) {            
+            var event = new CustomEvent("taskCompleted", {
+                detail: {
+                    message: "The task was completed!",
+                    time: new Date(),
+                },
+                bubbles: true,
+                cancelable: true
+            });
+            document.dispatchEvent(event);
+            showTimer();
+        }
+        return
+    }
+    secondsTimer -= 1
+    showTimer();
+}
+
+startButton.addEventListener('click', startAndPause);
+
+function startAndPause(){
+    if(timerBreak){
+        soundPause.play();
+        inputMusicFokus.checked = false;
+        music.pause();
+        stopCounter();
+        return
+    }
+    soundPlay.play();
+    timerBreak = setInterval(countDown, 1000);
+    startOrPauseButton.textContent = 'Pause';
+    imgPause.src = '/imagens/pause.png';
+}
+
+function stopCounter(){
+    clearInterval(timerBreak);
+    timerBreak = null;  
+    startOrPauseButton.textContent = 'Start';
+    imgPause.src = '/imagens/play_arrow.png';
+
+}
+
+function reset(){
+    buttonsList.forEach(button =>{
+        if(button.classList.contains('active')) {
+            secondsTimer = button.getAttribute('minutes') * 60;
+            showTimer();
+        }
+
+    }) 
+}
+
+function showTimer() {
+    console.log('seconds ' + secondsTimer)
+    const time = new Date(secondsTimer * 1000);
+    const formatTime = time.toLocaleTimeString('en', {minute:'2-digit', second: '2-digit'});
+    screenTimer.innerHTML = `${formatTime}`;
+}
+
+
+secondsTimer = buttonsList[0].getAttribute('minutes') * 60;
+showTimer();
+
